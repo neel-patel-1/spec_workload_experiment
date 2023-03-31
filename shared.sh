410483534c74a5d3bb8f7bc658f341550fdf75a5
Neel Patel <n869p538@york.ittc.ku.edu>
2023-03-31 18:33:31 -0500

    failed global variable for synch

--- shared.sh
+++ shared.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
+TEST="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 SPEC_ROOT=~/spec
 SPEC_OUTPUT=~/spec_out
 BACKGROUND_OUTPUT=~/spec_background
 BENCHS=( "cactuBSSN_s" "lbm_s" "xz_s" "mcf_s"  ) #memory intensive workloads
 CORES=( "0" "3" )
+export fin_spec=0
 
 build_bench(){
 	runcpu --action runsetup --output-root $SPEC_OUTPUT $1
+	runcpu --action runsetup --output-root $BACKGROUND_OUTPUT $1
+}
+clear_build(){
+	go $1
+	rm -rf build
 }
 
 build_all(){
@@ -19,15 +26,18 @@ build_all(){
 
 
 launch_bench(){
-	taskset -c $1 runcpu --action run --output-root $SPEC_OUTPUT $2
+	taskset -c $1 runcpu --nobuild --action run --output-root $SPEC_OUTPUT $2
 }
 launch_bench_onlyrun(){
-	taskset -c $1 runcpu --action onlyrun --output-root $BACKGROUND_OUTPUT $2
+	taskset -c $1 runcpu --nobuild --action onlyrun --output-root $BACKGROUND_OUTPUT $2
 }
 spec_proc(){
 	launch_bench $1 $2
 	# check for any reportable spec processes remaining
+	export fin_spec=$(( $finspec + 1 ))
+	echo "$2 finished in exported ${fin_spec}'d" | tee -a core_log
 	if [ ! "$fin_spec" -lt "${#CORES[@]}" ]; then
+		echo "$2 bench complete ..." | tee -a core_log
 		kill_all
 	else
 		launch_bench_onlyrun $1 $2
@@ -36,10 +46,11 @@ spec_proc(){
 }
 
 run_all(){
+	echo > core_log
 	for ((i=0;i<${#CORES[@]};++i)); do
 		bench=${BENCHS[i]}
 		core=${CORES[i]}
-		launch_bench $core $bench &
+		spec_proc $core $bench &
 	done
 }
 
@@ -59,3 +70,4 @@ function kill_all {
 
 cd $SPEC_ROOT
 source shrc
+cd $TEST
