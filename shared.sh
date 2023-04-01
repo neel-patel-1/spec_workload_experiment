005e2451700fe11c2dc723c09d28f574378f09c9
Neel Patel <n869p538@york.ittc.ku.edu>
2023-03-31 22:13:32 -0500

    workload_replicator 5 second sleep impl.

--- shared.sh
+++ shared.sh
@@ -4,9 +4,10 @@ TEST="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 SPEC_ROOT=~/spec
 SPEC_OUTPUT=~/spec_out
 BACKGROUND_OUTPUT=~/spec_background
-BENCHS=( "cactuBSSN_s" "lbm_s" "xz_s" "mcf_s"  ) #memory intensive workloads
-CORES=( "0" "3" )
-export fin_spec=0
+#BENCHS=( "cactuBSSN_s" "lbm_s" "xz_s" "mcf_s"  ) #memory intensive workloads
+BENCHS=(  "lbm_s" "xz_s" "mcf_s"  ) #memory intensive workloads
+#CORES=( "0" "1" "2" "3" )
+CORES=(  "1" "2" "3" )
 
 build_bench(){
 	runcpu --action runsetup --output-root $SPEC_OUTPUT $1
@@ -28,30 +29,35 @@ build_all(){
 launch_bench(){
 	taskset -c $1 runcpu --nobuild --action run --output-root $SPEC_OUTPUT $2
 }
-launch_bench_onlyrun(){
+launch_workload_replicator(){
+	while [ -e /proc/$3 ];
+	do
+		sleep 5
+	done
 	taskset -c $1 runcpu --nobuild --action onlyrun --output-root $BACKGROUND_OUTPUT $2
 }
-spec_proc(){
-	launch_bench $1 $2
-	# check for any reportable spec processes remaining
-	export fin_spec=$(( $finspec + 1 ))
-	echo "$2 finished in exported ${fin_spec}'d" | tee -a core_log
-	if [ ! "$fin_spec" -lt "${#CORES[@]}" ]; then
-		echo "$2 bench complete ..." | tee -a core_log
-		kill_all
-	else
-		launch_bench_onlyrun $1 $2
-	fi
 
+spec_proc(){
+	echo $$
+	wait
+
+	launch_bench_onlyrun $1 $2
 }
 
 run_all(){
-	echo > core_log
+	RUNNING_SPECS=()
+	echo > og_pids.txt
 	for ((i=0;i<${#CORES[@]};++i)); do
 		bench=${BENCHS[i]}
 		core=${CORES[i]}
-		spec_proc $core $bench &
+		2>1 >/dev/null launch_bench $core $bench & 
+		pid=$!
+		echo "$bench pid: $pid" | tee -a og_pids.txt
+		RUNNING_SPECS+=( "$pid" )
+		2>1 >/dev/null launch_workload_replicator $core $bench $pid &
 	done
+
+	echo ${RUNNING_SPECS[*]} | tee -a og_pids.txt
 }
 
 function kill_bench {
