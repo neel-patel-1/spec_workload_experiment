40bb1ff9b4de6815411fbaccc86e9094a84a6ca4
Neel Patel <n869p538@york.ittc.ku.edu>
2023-04-02 03:01:06 -0500

    fixup -- iterate through benchs/cores in workload_replicator assigment

--- shared.sh
+++ shared.sh
@@ -6,7 +6,7 @@ export SPEC_OUTPUT=$TEST/spec_out
 export BACKGROUND_OUTPUT=$TEST/spec_background
 
 SPEC_CORES=( `seq 1 3` )
-BENCHS=( "lbm_s" "xz_s" "mcf_s"  ) #memory intensive workloads
+BENCHS=( "lbm_s" "xz_s" "xz_s"  ) #memory intensive workloads
 #BENCHS=( "lbm_s" "xz_s" "xz_s"  ) #duplicate workload test
 #SPEC_CORES+=( `seq 11 13` )
 #BENCHS+=( "lbm_s" "xz_s" "mcf_s"  ) #memory intensive workloads
@@ -17,7 +17,7 @@ export MON_LOG=mon_log.txt
 export QZ_ROOT=$TEST/../QATzip
 export ANTAGONIST=$TEST/../antagonist.sh
 export ANTAGONIST_OUTPUT=$TEST/antagonist
-export COMP_CORES=(  "9" "19"  )
+export COMP_CORES=(  "0"  )
 
 export MON_CORE="0" #only check for workload completion and handle experiment termination
 
@@ -42,7 +42,7 @@ launch_reportable_specs(){
 		core=${SPEC_CORES[i]}
 		#2>1 >/dev/null taskset -c $core runcpu --nobuild --action run --output-root $SPEC_OUTPUT $bench &
 		taskset -c $core runcpu --nobuild --action run --output-root $SPEC_OUTPUT $bench 2>&1 | tee -a $SPEC_OUTPUT/spec_reportable_core_$core &
-		echo "run-cpu-initial-$bench: pid-$!" | tee -a $SPEC_LOG
+		echo "taskset -c $core runcpu --nobuild --action run --output-root $SPEC_OUTPUT $bench 2>&1 | tee -a $SPEC_OUTPUT/spec_reportable_core_$core &" | tee -a $MON_LOG
 	done
 }
 launch_workload_replicators(){
@@ -57,16 +57,18 @@ launch_workload_replicators(){
 	for i in "${uniq_benchs[@]}"; do
 		rem_pids=( $(pgrep $i) )
 		for ((j=0;j<${#BENCHS[@]};++j)); do
-			bench=${BENCHS[i]}
-			core=${SPEC_CORES[i]}
-			if [ "$i" -eq "$bench" ]; then
-				pid=${rem_pids[0]} 
-				rem_pids=( "${rem_pids[@]:0}" )
+			bench=${BENCHS[j]}
+			core=${SPEC_CORES[j]}
+			if [ "$i" == "$bench" ]; then
+				pid=${rem_pids[0]}
+				rem_pids=( ${rem_pids[@]:0} )
+				echo "remaining pids for $i : ${rem_pids[*]}" | tee -a $MON_LOG
 				[ -z "$pid" ] && echo "pid for $bench on core $core not found" | tee -a $MON_LOG && return -1
 				echo "assigning workload_replicator for $bench with pid $pid to core $core" | tee -a $MON_LOG
 
-				echo "taskset -c $MON_CORE ./workload_replicator.sh $core $bench $(pgrep $bench) &" | tee -a $MON_LOG
+				echo "taskset -c $MON_CORE ./workload_replicator.sh $core $bench $pid 2>&1 1>$SPEC_OUTPUT/workload_rep_core_$core &" | tee -a $MON_LOG
 				taskset -c $MON_CORE ./workload_replicator.sh $core $bench $pid 2>&1 1>$SPEC_OUTPUT/workload_rep_core_$core &
+				
 			fi
 		done
 	done
@@ -87,10 +89,12 @@ run_all(){
 
 	launch_antagonists
 
+	launch_reportable_specs
+
 	#sleep 10
 	launch_workload_replicators
 
-	taskset -c $MON_CORE ./experiment_terminator.sh
+	taskset -c $MON_CORE ./experiment_terminator.sh &
 
 }
 
