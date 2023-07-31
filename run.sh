06e7c70844193bf0261476b87040ba0542ded3a2
Neel Patel <n869p538@ittc.ku.edu>
2023-07-31 00:50:50 -0500

    update benchs

--- run.sh
+++ run.sh
@@ -13,6 +13,6 @@ echo "1" | sudo tee /sys/devices/system/cpu/intel_pstate/no_turbo
 build_all
 run_all_spec_no_replacement_no_antagonist
 
-mkdir -p perlbench8x_r2
-cp -r $SPEC_OUTPUT/result/* perlbench8x_r2
-cp antagonist_output/antagonist_log.txt perlbench8x_r2
+#mkdir -p perlbench8x_r2
+#cp -r $SPEC_OUTPUT/result/* perlbench8x_r2
+#cp antagonist_output/antagonist_log.txt perlbench8x_r2
