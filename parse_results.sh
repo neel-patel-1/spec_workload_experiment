96ecfd8a61d5e04c13ddc22b26d0240e9a94369c
nmpatel <nmpatel@node.nmpatel-163215.aifmevaluation-pg0.apt.emulab.net>
2023-08-09 03:58:02 -0600

    scale=4

--- parse_results.sh
+++ parse_results.sh
@@ -12,6 +12,7 @@ done | tee no_antagonist.txt
 
 n_val=$(awk '$4~/RE/{count--} { sum += $3;count++ } END{ print sum/(count-1)}' no_antagonist.txt)
 
+dir=$TEST/with_antagonist
 echo "Benchmarks      Threads  Run Time     Ratio" | tee with_antagonist.txt
 for bench in "${uniq_benchs[@]}"; do
 	ls $dir/*.txt | xargs -I {} sh -c "grep -e '$bench' {}| awk 'NF>3{print \$0}' | grep -v -e 'enough runs' -e'invalid' -e 'not valid' -e 'return' -e '-march' -e'No flags' | sort -d -t. -k2 | uniq " 
@@ -19,4 +20,6 @@ done | tee with_antagonist.txt
 
 a_val=$(awk '$4~/RE/{count--} { sum += $3;count++ } END{ print sum/(count-1)}' with_antagonist.txt)
 echo "Degradation(%):"
-echo "(($n_val-$a_val)/$n_val) * 100 "
+n_val=2300
+echo "(($a_val-$n_val)/$n_val) * 100 " 
+echo "scale=4; (($a_val- $n_val)/$n_val) * 100 " | bc
