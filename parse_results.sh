a33c721ee4a2c75bbba27e1d06222803616b34f4
nmpatel <nmpatel@node.nmpatel-163215.aifmevaluation-pg0.apt.emulab.net>
2023-08-10 03:26:15 -0600

    parse antagonist results

--- parse_results.sh
+++ parse_results.sh
@@ -9,7 +9,7 @@ echo "Benchmarks      Threads  Run Time     Ratio" | tee no_antagonist.txt
 for bench in "${uniq_benchs[@]}"; do
 	ls $dir/*.txt | xargs -I {} sh -c "grep -e '$bench' {}| awk 'NF>3{print \$0}' | grep -v -e 'enough runs' -e'invalid' -e 'not valid' -e 'return' -e '-march' -e'No flags' | sort -d -t. -k2 | uniq " 
 done | tee no_antagonist.txt
-
+n_ant=$(grep PAGEWRKR ${dir}/antagonist.log | grep -v '-' | cut -d ':' -f 5 | awk '{sum+=$1} END{print sum/NR}' )
 n_val=$(awk '$4~/RE/{count--} { sum += $3;count++ } END{ print sum/(count-1)}' no_antagonist.txt)
 
 dir=$TEST/with_antagonist
@@ -18,7 +18,11 @@ for bench in "${uniq_benchs[@]}"; do
 	ls $dir/*.txt | xargs -I {} sh -c "grep -e '$bench' {}| awk 'NF>3{print \$0}' | grep -v -e 'enough runs' -e'invalid' -e 'not valid' -e 'return' -e '-march' -e'No flags' | sort -d -t. -k2 | uniq " 
 done | tee with_antagonist.txt
 
+a_ant=$(grep PAGEWRKR ${dir}/antagonist.log | grep -v '-' | cut -d ':' -f 5 | awk '{sum+=$1} END{print sum/NR}' )
 a_val=$(awk '$4~/RE/{count--} { sum += $3;count++ } END{ print sum/(count-1)}' with_antagonist.txt)
-echo "Degradation(%):"
+echo "SPEC Degradation(%):"
 echo "(($a_val-$n_val)/$n_val) * 100 " 
 echo "scale=4; (($a_val- $n_val)/$n_val) * 100 " | bc
+echo "(De)compression Degradation(%):"
+echo "(($a_ant-$n_ant)/$n_ant) * 100 " 
+echo "scale=4; (($a_ant-$n_ant)/$n_ant) * 100 " | bc
--- run.sh
+++ run.sh
@@ -15,12 +15,14 @@ build_all
 run_all_spec_no_antagonist
 mkdir -p no_antagonist
 mv spec_out/result/* no_antagonist
+mv antagonist_output/antagonist_log.txt no_antagonist/antagonist.log
 kill_all_bench
 
 
 run_all_spec_with_antagonist
 mkdir -p with_antagonist
 mv spec_out/result/* with_antagonist
+mv antagonist_output/antagonist_log.txt with_antagonist/antagonist.log
 kill_all_bench
 
 ./parse_results.sh
